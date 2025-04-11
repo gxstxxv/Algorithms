ccc1dc9494f50df5a72f45e5d2a3e20925cff2b9
gxstxxv <144585373+gxstxxv@users.noreply.github.com>
2025-04-11 16:52:01 +0200

    implemented mergesort

--- MergeSort/main.go
+++ MergeSort/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
 
+func main() {
+	a := rand.Perm(40000)
+
+	start := time.Now()
+	a = MergeSort(a)
+	elapsed := time.Since(start)
+
+	fmt.Println(sort.IntsAreSorted(a))
+	fmt.Println(elapsed)
+}
+
+func MergeSort(a []int) []int {
+	if len(a) <= 1 {
+		return a
+	}
+
+	m := len(a) / 2
+	x := MergeSort(a[:m])
+	y := MergeSort(a[m:])
+
+	return merge(x, y)
+}
+
+func merge(x, y []int) []int {
+	r := make([]int, 0, len(x)+len(y))
+	i, j := 0, 0
+
+	for i < len(x) && j < len(y) {
+		if x[i] < y[j] {
+			r = append(r, x[i])
+			i++
+		} else {
+			r = append(r, y[j])
+			j++
+		}
+	}
+
+	r = append(r, x[i:]...)
+	r = append(r, y[j:]...)
+
+	return r
 }
